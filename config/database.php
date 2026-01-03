50844689dc545ceb84924c6247be31beb11d786b
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-01-03 21:16:24 +0100

    cree configuration de connection avec base de donnees

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'host' => 'localhost',
+    'dbname' => 'hotospherdb',
+    'username' => 'root',
+    'password' => '',
+    'options' => [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+];
