625a1c8782453bff8a80496251a3af470fc3927e
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-01-05 00:07:08 +0100

    ajouter des fonctions

--- App/Repositories/RepositoryInterface.php
+++ App/Repositories/RepositoryInterface.php
@@ -4,8 +4,11 @@ interface RepositoryInterface{
 
     public function findByUsername($username = null);
     public function findById($id);
-
-    //  public function add($user): bool;
+    public function add($user);
+    public function login($usernameInput, $passwordInput);
+    public function logout();
+    public function Update($user);
+    public function  delete($id);
     //  public function Update($user): array;
 }
 
