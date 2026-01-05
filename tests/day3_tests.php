b5b3763053f3e429230ef3b6194e68a98c845356
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-01-05 17:23:36 +0100

    resaudre problem de creation (ajouter des attribues au fonction create objects

--- tests/day3_tests.php
+++ tests/day3_tests.php
@@ -1,5 +1,7 @@
 <?php
 require_once __DIR__ . "/../App/Services/UserFactory.php";
+require_once __DIR__ . "/../App/Repositories/UserRepository.php";
+
 
 
 //test 
@@ -20,5 +22,48 @@ try {
 } catch (Exception $e) {
     echo "Erreur : " . $e->getMessage();
 }
+
+
+
+
+
+
+
+function FindUser(RepositoryInterface $repo) {
+   if($repo->findByUsername("amine_dev")) {
+       echo "User l'qinah f l'array!";
+   } else {
+       echo "User ma-kaynch.";
+   }
+}
+
+function FindUserid(RepositoryInterface $repo) {
+   if($repo->findById(2)) {
+       echo "User de id=1 l'qinah f l'array!";
+   } else {
+       echo "User ma-kaynch.";
+   }
+}
+
+$repo = new UserRepository();
+FindUser($repo);
+FindUserid($repo);
+echo "\n Avant \n";
+$repo->afficherAllUsers();
+$user1=new BasicUser('ahmed', 'oubelkacem.@gmail.com', '$2y$10$abcdefg12345', 'PHP Developer', '/img/amine.jpg', 'BasicUser', 3, '2025-12-15', NULL, 0);
+$repo->add($user1);
+echo " \nApret\n";
+$repo->afficherAllUsers();
+
+//  $repo->login("ahmed", "12345678"); //connected
+ $repo->delete(2); //delete sara
+ echo "\n";
+ $repo->afficherAllUsers();
+
+
+
+
+
+
 
 
