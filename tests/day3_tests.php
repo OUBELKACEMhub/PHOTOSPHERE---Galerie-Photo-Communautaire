bfa55f212f3224f960cc2e742f12260d974d7ad7
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-01-05 17:30:02 +0100

    tester les fonctions (add,delete,login,logout..)

--- tests/day3_tests.php
+++ tests/day3_tests.php
@@ -6,11 +6,11 @@ require_once __DIR__ . "/../App/Repositories/UserRepository.php";
 
 //test 
 $datauser = [
-    'username'             => "ahmed",
-    'email'                => "ahmed@gmail.com",
+    'username'             => "mounya",
+    'email'                => "mounya@gmail.com",
     'password_hash'        => "1223pass",
     'bio'                  => "Hi, I am an admin",
-    'profile_picture_path' => "pec/ahmed",
+    'profile_picture_path' => "pec/mounya",
     'role'                 => "Administrator", 
     'last_upload_reset_date' => null,
     'is_super_administrateur' => true
@@ -18,47 +18,49 @@ $datauser = [
 
 try {
     $userObj = UserFactory::create($datauser);
+
     echo "Utilisateur créé : " . $userObj->getUsername() . " avec le rôle " . $userObj->getRole();
 } catch (Exception $e) {
     echo "Erreur : " . $e->getMessage();
 }
 
-
-
-
-
-
-
 function FindUser(RepositoryInterface $repo) {
    if($repo->findByUsername("amine_dev")) {
-       echo "User l'qinah f l'array!";
+       echo "User found in array!";
    } else {
-       echo "User ma-kaynch.";
+       echo "User not found.";
    }
 }
 
 function FindUserid(RepositoryInterface $repo) {
    if($repo->findById(2)) {
-       echo "User de id=1 l'qinah f l'array!";
+       echo "User id=1 is found";
    } else {
-       echo "User ma-kaynch.";
+       echo " \n User id=1 is  Not found.";
    }
 }
+
+
+
 
 $repo = new UserRepository();
 FindUser($repo);
 FindUserid($repo);
 echo "\n Avant \n";
 $repo->afficherAllUsers();
-$user1=new BasicUser('ahmed', 'oubelkacem.@gmail.com', '$2y$10$abcdefg12345', 'PHP Developer', '/img/amine.jpg', 'BasicUser', 3, '2025-12-15', NULL, 0);
-$repo->add($user1);
+
+print_r($userObj);
+$userObj = UserFactory::create($datauser);
+$repo->add($userObj);
+
 echo " \nApret\n";
+
 $repo->afficherAllUsers();
 
-//  $repo->login("ahmed", "12345678"); //connected
- $repo->delete(2); //delete sara
- echo "\n";
- $repo->afficherAllUsers();
+// //  $repo->login("ahmed", "12345678"); //connected
+//  $repo->delete(2); //delete sara
+//  echo "\n";
+//  $repo->afficherAllUsers();
 
 
 
