407c3ae1e610feb9dcb88991fbca31c4365531e1
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-01-07 23:19:45 +0100

    ajouter des  functions de crud d'un album

--- App/Repositories/AlbumRepository.php
+++ App/Repositories/AlbumRepository.php
@@ -54,7 +54,7 @@ public function getAllUser(){
         $stmt->execute([':id' => $userId]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
         
-        return ($user && $user['role'] === 'ProUser');
+        return ($user && $user['role'] === 'ProUser')?: null;
     }
 
 
@@ -79,7 +79,7 @@ function findPost($id){
        $sql="SELECT * FROM postes where id=:id";
        $stmt=$pdo->prepare($sql);
        $stmt->execute([':id'=>$id]);
-      return  $stmt->fetch(PDO::FETCH_ASSOC);
+      return  $stmt->fetch(PDO::FETCH_ASSOC)?: null;
 }
   
  
@@ -88,25 +88,96 @@ function findAlbum($id){
        $sql="SELECT * FROM album where id=:id";
        $stmt=$pdo->prepare($sql);
        $stmt->execute([':id'=>$id]);
-      return  $stmt->fetch(PDO::FETCH_ASSOC);
+      return  $stmt->fetch(PDO::FETCH_ASSOC)?: null;
 } 
 
 
 
- public function addPhotoToAlbum(int $album_id, int $postId, int $userId): bool{
-              $post=findPost($postId);
-     $sql = "UPDATE postes set album_id=:album_id,updated_at=NOW() where id=:id";
+ public function addPostToAlbum(int $album_id, int $postId, int $userId): bool{
+           $pdo = Database::getConnection();
+            $post=$this->findPost($postId);
+            $sql = "UPDATE postes set album_id=:album_id,updated_at=NOW() where id=:id";
             $stmt = $pdo->prepare($sql);
             $stmt->execute([
                 ':album_id'     => $album_id,
                 ':id'       => $postId
             ]);
             echo "ajouter Post sur avec succees!";  
+    }  
 
- }  
 
+
+public function removePhotoFromAlbum($albumId, $photoId, $userId): bool 
+{
+    try {
+        $pdo = Database::getConnection();
+        $sql = "UPDATE postes  SET album_id = NULL  WHERE id = :photo_id 
+                AND album_id = :album_id  AND user_id = :user_id";
+
+        $stmt = $pdo->prepare($sql);
+        
+        $success = $stmt->execute([
+            ':photo_id' => $photoId,
+            ':album_id' => $albumId,
+            ':user_id'  => $userId
+        ]);
+
+        return $success && $stmt->rowCount() > 0;
+    } catch (PDOException $e) {
+        error_log("Erreur removePhotoFromAlbum : " . $e->getMessage());
+        return false;
+    }
+}
+
+public function getAlbumWithPhotos($userId,$includePrivate=true){
+ $pdo = Database::getConnection();
+        $sql = "SELECT * FROM album ab JOIN postes p 
+          ON ab.id=p.album_id where ab.visibility=:visibility";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['visibility'=>$includePrivate]);
+        return $stmt->fetch(PDO::FETCH_ASSOC)?: null;
+}
+
+public function updateAlbum(int $albumId, int $userId, array $data): bool{
+     if (!$this->isA_ProUser($userId) && $data['visibility'] != 'public') {
+            return false;
+        }
+ $pdo = Database::getConnection();
+        $sql = "UPDATE album  SET title=:title ,description=:description,visibility=:visibility
+        WHERE id=:id ";
+         $stmt = $pdo->prepare($sql);
+         $stmt->execute([
+            'title'=>$data['title'],
+            'description'=>$data['description'],
+            'visibility'=>$data['visibility']
+            ]);
+   return true;
+}
+
+public function deleteAlbum($albumId,$userId):bool{
+    try{
+         $pdo = Database::getConnection();
+        $sql="DELETE FROM albums WHERE id=:id";
+         $stmt = $pdo->prepare($sql);
+         $stmt->execute(['id'=>$albumId]);
+    }catch(PDOExecption $e){
+        echo "error  deleting field" . $e->getMessage();
+    }
+}
 }
 
 
+
+
+
+
+
+
+
+
+
+ //test
+
 $obj = new AlbumRepository();
-$obj->createAlbum(2, "ALBUM3", "no desc", "privee");
+ $obj->createAlbum(2, "ALBUM3", "no desc", "privee"); 
+$obj->addPhotoToAlbum(1,1,1);
