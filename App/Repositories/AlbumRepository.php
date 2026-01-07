c56021cd921bea1c93c6bfe7cf569a3c27bb98b7
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-01-07 16:38:33 +0100

    ajouter function getAllalbums pour manipuler le data sur la DB

---
+++ App/Repositories/AlbumRepository.php
@@ -0,0 +1,111 @@
+<?php
+require_once __DIR__ . "/../Entities/poste.php";
+require_once __DIR__ . "/../Entities/album.php";
+require_once __DIR__ . "/../Repositories/UserRepository.php";
+require_once __DIR__ . "/../../config/Core/Database.php";
+
+
+
+
+class AlbumRepository{
+
+
+public function __construct(){}
+public function getAllAlbums(): array {
+    try{
+    $pdo = Database::getConnection();
+    $sql = "SELECT * FROM album";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }catch(PDOException $e){
+      echo "error data not found" . $e->getMessage();
+    }
+}
+
+
+
+public function Title_is_Unique($title):bool{
+     $tab=$this->getAllAlbums();
+     $tr=0;
+     foreach($tab as $album){
+        if($album['title']==$title){
+            $tr=1;
+        }
+     }
+    return  $tr==1 ? false : true ;
+}
+
+
+public function getAllUser(){
+    try{
+     $pdo = Database::getConnection();
+            $sql = "SELECT * FROM users";
+            $stmt = $pdo->query($sql);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }catch(PDOException $e){
+      echo "error field to manipulate data!" . $e->getMessage();
+    }
+}
+
+ public function isA_ProUser(int $userId): bool {
+        $pdo = Database::getConnection();
+        $sql = "SELECT role FROM users WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([':id' => $userId]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        return ($user && $user['role'] === 'ProUser');
+    }
+
+
+ public function  createAlbum(int $userId, string $title, string $description, string $visibility): int{
+    try{
+       $pdo = Database::getConnection();
+       if (!$this->isA_ProUser($userId)) {
+            $visibility = 'public';
+        }
+
+      if ($this->Title_is_Unique($title)) {
+           $album=new Album ( $userId,  $title,  $description,  $visibility);
+    }
+}catch(PDOException $e){
+    echo "error field manipuling data!" . $e->getMessage();
+    }
+    
+     }
+
+function findPost($id){
+       $pdo = Database::getConnection();
+       $sql="SELECT * FROM postes where id=:id";
+       $stmt=$pdo->prepare($sql);
+       $stmt->execute([':id'=>$id]);
+      return  $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function findAlbum($id){
+       $pdo = Database::getConnection();
+       $sql="SELECT * FROM album where id=:id";
+       $stmt=$pdo->prepare($sql);
+       $stmt->execute([':id'=>$id]);
+      return  $stmt->fetch(PDO::FETCH_ASSOC);
+} 
+
+
+
+ public function addPhotoToAlbum(int $album_id, int $postId, int $userId): bool{
+              $post=findPost($postId);
+     $sql = "UPDATE postes set album_id=:album_id,updated_at=NOW() where id=:id";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([
+                ':album_id'     => $album_id,
+                ':id'       => $postId
+            ]);
+            echo "ajouter Post sur album avec succees!";  
+ }     
+
+}
+
+
+$obj = new AlbumRepository();
+$obj->createAlbum(2, "ALBUM3", "no desc", "privee");
